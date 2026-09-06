e2d14e3ff22398945fe9d1992d09e33104638994
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-09-06 12:17:47 +0300

    Load Portal 0.3 authentication styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
+import './auth.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
