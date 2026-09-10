d91bbba3d5464b8ca6ad8906ca1572732f79f6fa
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-09-10 10:48:50 +0300

    Expose RM3.6 platform profiles page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import BaselineReview from './BaselineReview';
+import PlatformProfiles from './PlatformProfiles';
 import './styles.css';
 import './auth.css';
 
@@ -17,12 +18,12 @@ const queryClient = new QueryClient({
   }
 });
 
-const baselineReview = window.location.pathname === '/baselines';
+const standalone = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      {baselineReview ? <BaselineReview /> : <BrowserRouter><App /></BrowserRouter>}
+      {standalone === '/baselines' ? <BaselineReview /> : standalone === '/platform-profiles' ? <PlatformProfiles /> : <BrowserRouter><App /></BrowserRouter>}
     </QueryClientProvider>
   </React.StrictMode>
 );
